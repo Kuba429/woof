8fac6d8644941bfd56968f8d266f6ceec3a9593f
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-13 11:39:24 +0200

    wszystkie rasy psow

--- src/views/all/All.tsx
+++ src/views/all/All.tsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import { Layout } from "../../components/Layout";
+
 export function AllView() {
-	return <div>all</div>;
+	const breeds = useFetchAllBreeds();
+	return (
+		<Layout>
+			{breeds.map((breed) => (
+				<p>{breed}</p>
+			))}
+		</Layout>
+	);
+}
+
+type rawBreedsType = {
+	[breed: string]: string[];
+};
+
+function useFetchAllBreeds() {
+	const [state, setState] = useState<string[]>([]);
+	async function asyncEffect() {
+		const res = await fetch("https://dog.ceo/api/breeds/list/all");
+		const json = await res.json();
+		const rawBreeds = json.message as rawBreedsType;
+		const entries = Object.entries(rawBreeds);
+		const breeds = entries.flatMap(([breed, subBreeds]) =>
+			subBreeds.map((sub) => `${sub} ${breed}`)
+		);
+		setState(breeds);
+	}
+	useEffect(() => {
+		asyncEffect();
+	}, [setState]);
+	return state;
 }
