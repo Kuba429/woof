61f395d4af411e18d706be7a32c8eee5e5dfaee8
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-14 15:27:02 +0200

    sprawdz czy rasa zostala polubiona

--- src/stores/breeds.ts
+++ src/stores/breeds.ts
@@ -2,6 +2,7 @@ import { atom } from "jotai";
 
 async function fetchAllBreeds() {
 	// const res = await fetch("https://dog.ceo/api/breeds/list/all");
+	const liked = getLikedBreeds();
 	const res = await fetch("/backup.json");
 	type rawBreedsType = { [breed: string]: string[] };
 	const rawBreeds = (await res.json()).message as rawBreedsType;
@@ -11,10 +12,17 @@ async function fetchAllBreeds() {
 			main,
 			sub,
 			image: undefined as string | undefined, // zapamiętuję url do obrazu aby nie wysyłać tego samego zapytania za każdym razem bez potrzeby
-			liked: false,
+			isLiked: liked.some(
+				([itemMain, itemSub]) => itemMain === main && itemSub === sub
+			),
 		}))
 	);
 	return breeds;
+}
+function getLikedBreeds(): [string, string][] {
+	const likedJson = localStorage.getItem("liked-breeds");
+	if (!likedJson) return [];
+	return JSON.parse(likedJson);
 }
 export const allBreedsAtom = atom(await fetchAllBreeds());
 export const setImageAtom = atom(
