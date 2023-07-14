711f7465b4e953d6737a2362832881de44d641ab
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-14 14:51:22 +0200

    zapisywanie zdjec konkretnej rasy

--- src/stores/breeds.ts
+++ src/stores/breeds.ts
@@ -7,8 +7,28 @@ async function fetchAllBreeds() {
 	const rawBreeds = (await res.json()).message as rawBreedsType;
 	// konwertowanie ras do odpowiedniej formy
 	const breeds = Object.entries(rawBreeds).flatMap(([main, sub]) =>
-		sub.map((sub) => ({ main, sub }))
+		sub.map((sub) => ({
+			main,
+			sub,
+			image: undefined as string | undefined, // zapamiętuję url do obrazu aby nie wysyłać tego samego zapytania za każdym razem bez potrzeby
+			liked: false,
+		}))
 	);
 	return breeds;
 }
 export const allBreedsAtom = atom(await fetchAllBreeds());
+export const setImageAtom = atom(
+	null,
+	(get, set, update: { main: string; sub: string; newImage: string }) => {
+		console.log(update.newImage);
+		const copy = [...get(allBreedsAtom)];
+		const itemToUpdate = copy.find(
+			(item) => item.sub === update.sub && item.main === update.main
+		);
+		if (!itemToUpdate) {
+			return;
+		}
+		itemToUpdate.image = update.newImage;
+		set(allBreedsAtom, copy);
+	}
+);
--- src/views/breed/BreedView.tsx
+++ src/views/breed/BreedView.tsx
@@ -1,20 +1,27 @@
 import styles from "./Breed.module.scss";
 import { useParams } from "react-router-dom";
 import { Layout } from "../../components/Layout";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { allBreedsAtom, setImageAtom } from "../../stores/breeds";
 
 export function BreedView() {
 	const params = useParams();
-	const breed = (params.sub ?? "") + " " + params.main;
-	const [imgUrl, setImgUrl] = useState("/vite.svg");
+	const [allBreeds] = useAtom(allBreedsAtom);
+	const [, setImage] = useAtom(setImageAtom);
+	const breed = allBreeds.find(
+		(item) => item.main === params.main && item.sub === params.sub
+	)!; // TODO
+	const breedName = (breed.sub ?? "") + " " + breed.main;
 	useEffect(() => {
 		async function fetchImgUrl() {
 			const res = await fetch(
 				`https://dog.ceo/api/breed/hound/images/random`
 			);
 			const url = (await res.json()).message;
-			setImgUrl(url);
+			setImage({ sub: breed.sub, main: breed.main, newImage: url });
 		}
+		if (breed && breed.image) return;
 		fetchImgUrl();
 	}, []);
 	return (
@@ -22,10 +29,10 @@ export function BreedView() {
 			<div className={styles.container}>
 				<img
 					className={styles.image}
-					src={imgUrl}
-					alt={breed + " image"}
+					src={breed.image}
+					alt={breedName + " image"}
 				/>
-				<h2 className={styles.header}>{breed}</h2>
+				<h2 className={styles.header}>{breedName}</h2>
 				<p>
 					Ten pies to wierny i przyjacielski czworonóg, który świetnie
 					czuje się w roli rodzinnego towarzysza. Dobrze dogaduje się
