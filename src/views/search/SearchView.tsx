98f5da6c8a89cb807ce3e91e42d0e4eb08221815
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-15 14:22:11 +0200

    label dla inputu szukania

--- src/views/search/SearchView.tsx
+++ src/views/search/SearchView.tsx
@@ -1,19 +1,26 @@
-import { FormEvent } from "react";
+import { FormEvent, useId } from "react";
 import styles from "./Search.module.scss";
 export function SearchView() {
 	const handleSubmit = (e: FormEvent) => {
 		e.preventDefault();
 		// TODO
 	};
+	const inputId = useId();
 	return (
 		<>
 			<h2 className={styles.header}>Szukaj a znajdziesz ;)</h2>
 			<form onSubmit={handleSubmit} className={styles.form}>
-				<input
-					className={styles.input}
-					placeholder="np. Bulldog"
-					type="text"
-				/>
+				<div className={styles.inputWrapper}>
+					<input
+						id={inputId}
+						className={styles.input}
+						placeholder="np. Bulldog"
+						type="text"
+					/>
+					<label htmlFor={inputId}>
+						Wpisz nazwę rasy, której szukasz
+					</label>
+				</div>
 				<button type="submit" className={styles.button}>
 					Szukaj
 				</button>
