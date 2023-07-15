2f8e0361b2bc971d7c1c429ac7f2ea3e427e015c
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-15 13:59:06 +0200

    podstrona do szukania; input

--- src/views/search/SearchView.tsx
+++ src/views/search/SearchView.tsx
@@ -1,3 +1,23 @@
+import { FormEvent } from "react";
+import styles from "./Search.module.scss";
 export function SearchView() {
-	return <div>search</div>;
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		// TODO
+	};
+	return (
+		<>
+			<h2 className={styles.header}>Szukaj a znajdziesz ;)</h2>
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<input
+					className={styles.input}
+					placeholder="np. Bulldog"
+					type="text"
+				/>
+				<button type="submit" className={styles.button}>
+					Szukaj
+				</button>
+			</form>
+		</>
+	);
 }
