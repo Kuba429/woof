65e449bfe7af4e63e2c2d1d5c3184af3001fa50f
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-16 13:54:57 +0200

    animacje przy zmianie strony

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import { SearchView } from "./views/search/SearchView";
 import { AllView } from "./views/all/AllView";
 import { BreedView } from "./views/breed/BreedView";
 import { LikedView } from "./views/liked/LikedView";
 import { Layout } from "./components/Layout";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function App() {
 	const element = useRoutes([
@@ -28,5 +29,27 @@ export function App() {
 			element: <LikedView />,
 		},
 	]);
-	return <Layout>{element}</Layout>;
+	const location = useLocation();
+	return (
+		<Layout>
+			<AnimatePresence mode="wait">
+				<motion.div
+					key={location.pathname}
+					initial={{ x: -500, opacity: 0 }}
+					animate={{
+						x: 0,
+						opacity: 1,
+						transition: { duration: 0.2 },
+					}}
+					exit={{
+						x: -500,
+						opacity: 0,
+						transition: { duration: 0.2 },
+					}}
+				>
+					{element}
+				</motion.div>
+			</AnimatePresence>
+		</Layout>
+	);
 }
