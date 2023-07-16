00fe0d7df035fed5343a9d8a750a41f24a43df1a
Kuba429 <jakubzawadzki429@gmail.com>
2023-07-16 13:37:47 +0200

    tekst zastepczy w przypadku pustej listy ulubionych ras

--- src/views/liked/LikedView.tsx
+++ src/views/liked/LikedView.tsx
@@ -5,5 +5,21 @@ import { BreedList } from "../../components/BreedList";
 export function LikedView() {
 	const [breeds] = useAtom(allBreedsAtom);
 	const likedBreeds = breeds.filter((item) => item.isLiked);
-	return <BreedList header={"Ulubione"} items={likedBreeds} />;
+	return (
+		<BreedList
+			fallback={fallback}
+			header={"Ulubione"}
+			items={likedBreeds}
+		/>
+	);
 }
+
+const fallback = (
+	<div style={{ textAlign: "center" }}>
+		<h1>Nic tu nie ma</h1>
+		<p>
+			Kliknij w serduszko obok rasy jaka Ci się podoba aby ją polubić.
+			Polubione możesz znaleźć tutaj.
+		</p>
+	</div>
+);
